b6a984a30559e71bbd11c087bdca44d65022962c
Garrett Dunlap <g.e.dunlap90@gmail.com>
2021-02-27 13:17:05 -0500

    start build function for ajax pull with user input

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const $title = $('#name')
 const $desc = $('#desc')
+const $input = $('input[type="text"]')
 
 $('form').on('submit', handleGetData);
 
 function handleGetData(event) {
     event.preventDefault();
+
+    userInput = $input.val().replace(/ /g, '-');
+
+    console.log(userInput);
+
+    $.ajax({
+        url:'https://api.rawg.io/api/games/' + userInput
+    }).then({})
 }
