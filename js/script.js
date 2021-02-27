bd08b243e7193fbc8c8132cad9b422620c407a74
Garrett Dunlap <g.e.dunlap90@gmail.com>
2021-02-27 15:56:32 -0500

    setting up second clickfunction

--- js/script.js
+++ js/script.js
@@ -28,11 +28,20 @@ function handleGetData(event) {
         (error) => {
             console.log(`That's not quite right: `, error);
         }
-    )
+    );
+
+    // $('.titleimg').html('<img src="https://media.rawg.io/media/games/0bc/0bcc108295a244b488d5c25f7d867220.jpg"');
 }
 
+$('button').on('click', handleGetData2);
+
+function handleGetData2(event) {
+    event.preventDefault();
+    
+}
 
 function render() {
+    $titleimg.text(gameData.background_image);
     $title.text(gameData.name);
     $desc.text(gameData.description_raw);
     $date.text(gameData.released);
